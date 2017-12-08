f4cb23de689c756f0ba47afa2e126db1e85b5ab7
William <William@163.com>
2017-12-08 19:53:42 +0800

    更新代码

--- 畅通工程之局部最小花费问题/畅通工程之局部最小花费问题/main.c
+++ 畅通工程之局部最小花费问题/畅通工程之局部最小花费问题/main.c
@@ -7,9 +7,54 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX 10000
+//定义Prim算法求解最小生成树
+void Prim(int gm[MAX][MAX],int tree[MAX],int cost[MAX],int gmSize){
+    int *flag = (int *)malloc(sizeof(int) * gmSize);
+    int mincost,k = 0;
+    for(int i = 0;i<gmSize;i++){
+        cost[i] = gm[0][i];
+        tree[i] = 0;
+        flag[i] = 0;
+    }
+    flag[0] = 1;
+    
+    for(int i = 1;i<gmSize;i++){
+        mincost = 100000;          //将mincost赋值一个很大的值
+        for(int j = 1;j<gmSize;j++){
+            if(flag[j] == 0 && cost[j] < mincost){
+                mincost = cost[j];
+                k = j;
+            }
+        }
+        flag[k] = 1;
+        
+        for(int j = 1;j<gmSize;j++){
+            if(flag[j] == 0 && gm[k][j] < cost[j]){
+                cost[j] = gm[k][j];
+                tree[j] = k;
+            }
+        }
+    }
+    
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int m,n;
+    scanf("%d %d",&m,&n);
+    //定义图的邻接矩阵存储
+    int **cities = (int **)malloc(sizeof(int *) * n);
+    for(int i = 0;i<n;i++){
+        cities[i] = (int *)malloc(sizeof(int) * m);
+    }
+    
+    
+    
+    
+    
     return 0;
 }
+
+
+
