f77360d292e867afc448dd7943dc94a934c17139
William <William@163.com>
2017-12-09 12:21:17 +0800

    更新了代码

--- 畅通工程之局部最小花费问题/畅通工程之局部最小花费问题/main.c
+++ 畅通工程之局部最小花费问题/畅通工程之局部最小花费问题/main.c
@@ -11,23 +11,20 @@
 #define MAX 10000
 #define MAX_VALUE 1e+8
 //定义Prim算法求解最小生成树
-int Prim(int **cities,int **untouch,int size){
+int Prim(int **cities,int **touchState,int size){
     int sumMincost = 0;
     int *cost = (int *)malloc(sizeof(int) * size);
     int *flag = (int *)malloc(sizeof(int) * size);
     int mincost,k = 0;
-    
     //将第一个作为开始节点
     for(int i = 0;i<size;i++){
         int isTouch = 1;
         static int firstEle = 0;
         //记录初始联通的节点个数
         int touchCount = 0;
-//        tree[i] = 0;
-        
-        
+        //判断初始是否有联通的节点
         for(int j = 0;j<size;j++){
-            if(untouch[i][j] == 1){
+            if(touchState[i][j] == 1){
                 isTouch = 1;
                 break;
             }else{
@@ -45,9 +42,6 @@ int Prim(int **cities,int **untouch,int size){
         printf("flag[%d] = %d\n",i,flag[i]);
         cost[i] = cities[firstEle][i];
     }
-
-    
-    
     for(int i = 1;i<size;i++){
         mincost = MAX_VALUE;          //将mincost赋值一个很大的值
         for(int j = 1;j<size;j++){
@@ -58,7 +52,6 @@ int Prim(int **cities,int **untouch,int size){
         }
         //找出最小花费的路线
         flag[k] = 1;
-        
         for(int j = 1;j<size;j++){
             if(flag[j] == 0 && cities[k][j] < cost[j]){
                 cost[j] = cities[k][j];
@@ -71,30 +64,29 @@ int Prim(int **cities,int **untouch,int size){
     }
     return sumMincost;
 }
-
 int main(int argc, const char * argv[]) {
     int m = 0;
     //定义城市间使用图的邻接矩阵存储以及未联通城市的存储
-    int **cities , **untch;
+    int **cities , **touchState;
     scanf("%d",&m);
     //邻接矩阵存储
-    untch = (int **)malloc(sizeof(int *) * m);
+    touchState = (int **)malloc(sizeof(int *) * m);
     //邻接矩阵存储
     cities = (int **)malloc(sizeof(int *) * m);
     for(int i = 0;i<m;i++){
         cities[i] = (int *)malloc(sizeof(int) * m);
-        untch[i] = (int *)malloc(sizeof(int) * m);
+        touchState[i] = (int *)malloc(sizeof(int) * m);
     }
     for(int i = 0;i<m;i++){
         for(int j = 0;j<m;j++){
             cities[i][j] = 0;
-            untch[i][j] = 0;
+            touchState[i][j] = 0;
         }
     }
     
 //    for(int i = 0;i<m;i++){
 //        for(int j = 0;j<m;j++){
-//            printf("%d,%d\n",cities[i][j],untch[i][j]);
+//            printf("%d,%d\n",cities[i][j],touchState[i][j]);
 //        }
 //    }
     //输入数据总数
@@ -111,17 +103,17 @@ int main(int argc, const char * argv[]) {
         cities[a][b] = cities[b][a] = temp[2];
         //记录未联通的点(若联通赋值为1)
         if(temp[3] != 0){
-            untch[a][b] = untch[b][a] = 1;
+            touchState[a][b] = touchState[b][a] = 1;
         }
     }
     
 //    for(int i = 0;i<m;i++){
 //        for(int j = 0;j<m;j++){
-//            printf("city%d-city%d : cost = %d :isTouch = %d\n", i+1,j+1,cities[i][j],untch[i][j]);
+//            printf("city%d-city%d : cost = %d :isTouch = %d\n", i+1,j+1,cities[i][j],touchState[i][j]);
 //        }
 //    }
     
-    printf("%d",Prim(cities, untch, m));
+    printf("%d",Prim(cities, touchState, m));
 
     
 /*
