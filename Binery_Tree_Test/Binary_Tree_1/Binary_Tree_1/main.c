89c2593d706ffff958e29ae1ef8cd819da7ebad8
William <William@163.com>
2017-12-14 15:11:34 +0800

    基本框架

--- Binery_Tree_Test/Binary_Tree_1/Binary_Tree_1/main.c
+++ Binery_Tree_Test/Binary_Tree_1/Binary_Tree_1/main.c
@@ -16,16 +16,48 @@ typedef struct node{
     struct node *lchild;
     struct node *rchild;
 }BNode,*BiTree;
-//递归算法
+
+/*
+ 递归算法
+ */
+
+//1.节点个数
+int TreeNodeNum_Recursion(BiTree t){
+    return 0;
+}
+//2.树的深度
 int TreeHeight_Recursion(BiTree t){
     return 0;
 }
-
-//非递归算法
-int TreeHight_Non_Recursion(BiTree t){
+//3.各节点层数
+int TreeNodeTier_Recursion(BiTree t){
+    return 0;
+}
+//4.交换左右子树
+int TransLRTree_Recursion(BiTree t){
     return 0;
 }
 
+/*
+ 非递归算法
+ */
+
+//1.节点个数
+int TreeNodeNum_Non_Recursion(BiTree t){
+    return 0;
+}
+//2.树的深度
+int TreeHeight_Non_Recursion(BiTree t){
+    return 0;
+}
+//3.各节点层数
+int TreeNodeTier_Non_Recursion(BiTree t){
+    return 0;
+}
+//4.交换左右子树
+int TransLRTree_Non_Recursion(BiTree t){
+    return 0;
+}
 int main(int argc, const char * argv[]) {
     
     return 0;
