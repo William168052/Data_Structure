bc81ac68bfc8ab238551cec2c1e0f0b23f50139b
William <William@163.com>
2017-12-14 15:02:30 +0800

    更新了代码

---
+++ Binery_Tree_Test/Binary_Tree_1/Binary_Tree_1/main.c
@@ -0,0 +1,32 @@
+//
+//  main.c
+//  Binary_Tree_1
+//
+//  Created by William on 2017/12/14.
+//  Copyright © 2017年 William. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#define DataType int
+
+//定义二叉树节点结构
+typedef struct node{
+    DataType data;
+    struct node *lchild;
+    struct node *rchild;
+}BNode,*BiTree;
+//递归算法
+int TreeHeight_Recursion(BiTree t){
+    return 0;
+}
+
+//非递归算法
+int TreeHight_Non_Recursion(BiTree t){
+    return 0;
+}
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
