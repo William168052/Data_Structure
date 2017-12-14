baa66c9b63878fdbab31b9e0268b93d7d12a6697
William <William@163.com>
2017-12-14 15:32:15 +0800

    更新了代码

--- Binery_Tree_Test/Binary_Tree_1/Binary_Tree_1/main.c
+++ Binery_Tree_Test/Binary_Tree_1/Binary_Tree_1/main.c
@@ -23,19 +23,51 @@ typedef struct node{
 
 //1.节点个数
 int TreeNodeNum_Recursion(BiTree t){
-    return 0;
+    int nodeNum = 0;
+    if(t!=NULL){
+        //计算左、右子树节点数
+        nodeNum += TreeNodeNum_Recursion(t->lchild);
+        nodeNum += TreeNodeNum_Recursion(t->rchild);
+        return nodeNum+1;
+    }else{
+        return 0;
+    }
+    
 }
 //2.树的深度
 int TreeHeight_Recursion(BiTree t){
+    int treeHeight = 0;
+    if(t != NULL){
+        treeHeight++;
+        //计算左右子树的深度
+        int lHeight = TreeHeight_Recursion(t->lchild);
+        int rHeight = TreeHeight_Recursion(t->rchild);
+        treeHeight += lHeight > rHeight ? lHeight : rHeight;
+        return treeHeight;
+    }else{
+        return 0;
+    }
     return 0;
 }
 //3.各节点层数
 int TreeNodeTier_Recursion(BiTree t){
+    
     return 0;
 }
-//4.交换左右子树
-int TransLRTree_Recursion(BiTree t){
-    return 0;
+//4.交换左右子树(注意：不能用中序次序交换)
+void TransLRTree_Recursion(BiTree t){
+    if(t != NULL){
+        BiTree temp;
+        //先交换根的左右子树
+        temp = t->lchild;
+        t->lchild = t->rchild;
+        t->rchild = temp;
+        //再交换左右孩子的子树
+        TransLRTree_Recursion(t->lchild);
+        TransLRTree_Recursion(t->rchild);
+    }else{
+        return;
+    }
 }
 
 /*
