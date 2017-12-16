302aa10cd2a500e8a2c49a86212884ee983c5f6f
William <William@163.com>
2017-12-16 20:55:57 +0800

    更新了代码

--- Binery_Tree_Test/Binary_Tree_2/Binary_Tree_2/Binary_Tree_2/main.c
+++ Binery_Tree_Test/Binary_Tree_2/Binary_Tree_2/Binary_Tree_2/main.c
@@ -42,9 +42,9 @@ void push_Stack(int data,Stack *s){
 
 //出栈
 int pop_Stack(Stack *s){
-    if(s->top--!=-1){
+    if(s->top!=-1){
         s->length--;
-        return s->data[--s->top];
+        return s->data[s->top--];
     }
     return 0;
     
@@ -58,16 +58,16 @@ int getTop(Stack *s){
     return s->data[s->top];
 }
 
-
+//通过修改非递归先序遍历的算法实现
 void Pre_Order_complete_BiTree(BiTree t){
     if(t==NULL){
         return;
     }
     Stack *s = init_Stack();
     int i = 1;
-    while(!(i>t->nodeNum && s->top == -1)){
+    while(!(i>t->nodeNum && s->top==-1)){
         //若i所在的节点在数组中
-        while(i<=t->nodeNum){
+        while(i<t->nodeNum){
             printf("%d",t->data[i]);
             push_Stack(i, s);
             //访问其左孩子
@@ -76,11 +76,9 @@ void Pre_Order_complete_BiTree(BiTree t){
         //若不在数组中则出栈
         i = pop_Stack(s);
         i = i * 2 + 1;
-        
     }
 }
 
 int main(int argc, const char * argv[]) {
-    
     return 0;
 }
